3b6eca4d74de8baa7b8f91e828e6e45f9fdcdfd2
RinshaRahees <104898673+RinshaRahees@users.noreply.github.com>
2024-05-10 16:15:24 +0530

    Final commit & Animation add

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -14,37 +14,38 @@ function Background({ children }) {
   return (
     <div className="flex items-center justify-center h-screen w-full bg-cover bg-[url('/assets/bg.png')]" onMouseMove={parallax}>
       <img
-        className="parallax absolute top-10 left-24 w-24"
+        className="parallax absolute top-10 left-24 w-24 animate-pulse"
         src={new URL("/assets/rocket.png", import.meta.url).href}
         alt="Rocket"
         data-value="-5"
       />
       <img
-        className="parallax absolute bottom-12 left-10 w-24"
+        className="parallax absolute bottom-12 left-10 w-24 animate-pulse"
         src={new URL("/assets/galaxy.png", import.meta.url).href}
         alt="Galaxy"
         data-value="-3"
       />
+  
       <img
-        className="parallax absolute top-56 left-32 w-24"
+        className="parallax absolute top-56 left-32 w-24 animate-pulse"
         src={new URL("/assets/earth.png", import.meta.url).href}
         alt="Earth"
         data-value="2"
       />
       <img
-        className="parallax absolute top-44 right-44 w-56"
+        className="parallax absolute top-44 right-44 w-56 animate-pulse"
         src={new URL("/assets/planet.png", import.meta.url).href}
         alt="Planet"
         data-value="2"
       />
       <img
-        className="parallax absolute top-10 right-10 w-32"
+        className="parallax absolute top-10 right-10 w-32 animate-pulse"
         src={new URL("/assets/space.png", import.meta.url).href}
         alt="Space"
         data-value="3"
       />
       <img
-        className="parallax absolute bottom-10 right-24 w-24"
+        className="parallax absolute bottom-10 right-24 w-24 animate-pulse"
         src={new URL("/assets/solar-system.png", import.meta.url).href}
         alt="Solar System"
         data-value="4"
